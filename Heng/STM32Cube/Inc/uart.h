add889cbbadd9553b57937ed568668f178edf524
unknown <swerve0o0o0o@gmail.com>
2019-01-16 13:45:06 -0600

    inital code push

---
+++ Heng/STM32Cube/Inc/uart.h
@@ -0,0 +1,8 @@
+#include <stdint.h>
+
+	
+	void uart_send_message(char v);
+/*	char uart_receive_message(void);*/
+	void print_str(char * message);
+	
+
---
+++ Heng/STM32Cube/MDK-ARM/STM32Cube/startup_stm32f100xb.d
@@ -0,0 +1,1 @@
+stm32cube\startup_stm32f100xb.o: startup_stm32f100xb.s
