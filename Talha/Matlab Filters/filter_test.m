7d454270c995335f2fc52f5cacbb27e988d7009a
Talha Alvi <talha.alvi11@gmail.com>
2019-01-29 21:18:14 -0600

    update matlab scripts for filtering

--- Talha/Matlab Filters/filter_test.m
+++ Talha/Matlab Filters/filter_test.m
@@ -1,14 +1,18 @@
 load log3.dat
 load bump.dat
 logx = log3;
-n = 10;
-b = arrayfun(@(i) mean(logx(i:i+n-1)),1:n:length(logx)-n+1)';
-logx = b;
+
 figure(1)
-plot(log3);
-axis([0 25000 -2 4]);
-% axis([0 5000 1.1 1.3])
-% Fs = 1000;  % Sampling Frequency
+plot(logx);
+axis([0 25000 -1 5]);
+title('Accelerometer Sampled Data Unfiltered');
+xlabel('# of Samples');
+ylabel('Acceleration(g)');
+
+
+
+
+
 Fs = 1000;
 T = 1/Fs;
 L = length(logx);
@@ -19,27 +23,30 @@ P1(2:end-1) = 2*P1(2:end-1);
 f = Fs*(0:(L/2))/L;
 figure(2)
 plot(f, P1);
-axis([0 500 0 0.06])
+axis([0 500 0 0.1]);
+title('Frequency Spectrum of Unfiltered Accelerometer Data');
+xlabel('Frequency');
+ylabel('Magnitude');
 
-  % Sampling Frequency
 
 % Construct an FDESIGN object and call its BUTTER method.
 
-
-N   = 2;   % Order
+N   = 4;   % Order
 Fc1 = 3;   % First Cutoff Frequency
-Fc2 = 80;  % Second Cutoff Frequency
+Fc2 = 150;  % Second Cutoff Frequency
 
 % Construct an FDESIGN object and call its BUTTER method.
 h  = fdesign.bandpass('N,F3dB1,F3dB2', N, Fc1, Fc2, Fs);
 Hd = design(h, 'butter');
 y = filter(Hd, logx);
-figure(3)
+figure(3);
 plot(y)
-axis([0 3000 -1 1]);
+axis([0 25000 -3 3]);
+title('Bandpass (3 Hz - 150 Hz) Filtered Accelerometer Data');
+xlabel('# of Samples');
+ylabel('Acceleration(g)');
 
 
-Fs = 1000;
 T = 1/Fs;
 L = length(y);
 Y = fft(y);
@@ -49,8 +56,9 @@ P1(2:end-1) = 2*P1(2:end-1);
 f = Fs*(0:(L/2))/L;
 figure(4)
 plot(f, P1);
-axis([0 500 0 0.06])
+axis([0 500 0 0.1])
+title('Frequency Spectrum of Filtered Accelerometer Data');
+xlabel('Frequency');
+ylabel('Magnitude');
 
 
-
-%plot(line) 
