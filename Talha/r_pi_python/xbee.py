7bca19710187081af38c1584d16ca670d909bbfe
talha.alvi11@gmail.com <talha.alvi11@gmail.com>
2019-02-19 20:06:49 -0600

    writing data received from xbee in csv format

--- Talha/r_pi_python/xbee.py
+++ Talha/r_pi_python/xbee.py
@@ -8,9 +8,9 @@ start = timer()
 ser = serial.Serial("/dev/ttyUSB0", baudrate=115200, bytesize=8, parity='N', stopbits=1)
 
 to_send1 = [0x42];
-#to_send = [0x7E, 0x00, 0x04, 0x08, 0x01, 0x48, 0x56, 0x58]
+to_send = [0x7E, 0x00, 0x04, 0x08, 0x01, 0x48, 0x56, 0x58]
 ser.write(serial.to_bytes(to_send1))
-#ser.write(serial.to_bytes(to_send))
+ser.write(serial.to_bytes(to_send))
 
 while True:
    data = ser.read()
