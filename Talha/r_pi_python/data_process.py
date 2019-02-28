45228100e0d8dbba94822e4c556c0af501af9493
Talha <talha.alvi11@gmail.com>
2019-02-28 02:00:32 -0600

    parsed file to get acceleration data

--- Talha/r_pi_python/data_process.py
+++ Talha/r_pi_python/data_process.py
@@ -3,9 +3,12 @@ import csv
 
 def get_identity(seq):
      return seq[0:4]
+
 def split_element(seq, length):
       return [seq[i:i+length] for i in range(0, len(seq), length)]
+
 def check_sum_verify(seq):
+    seq = list(seq)
     sum = 0
     del seq[0:3]
     for x in range(len(seq)):
@@ -13,11 +16,18 @@ def check_sum_verify(seq):
     check = sum & 0xff
     return check == 0xff
 
+def shift_add(seq):
+    list_top = [seq[x]<<8 for x in range(0, len(seq), 2)]
+    list_bottom = [seq[x+1] for x in range(0, len(seq), 2)]
+    list = [(list_top[x] + list_bottom[x])/2048 for x in range(0, len(list_top))]
+    return list
+
 with open("filetest.csv", mode='r') as csvfile:
     my_list = []
     reader = csv.reader(csvfile)
     for row in reader:
         my_list.append(row)
+
 
 lists = [my_list[x][0] for x in range(0, len(my_list))]
 #list = [splitCount(lists[x]) for x in range(len(lists))]
@@ -29,20 +39,22 @@ for x in range(len(lists)):
         del temp_list[-1]
         temp_list.insert(0, '7e')
         list_2 = [int(temp_list[x], 16) for x in range(0, len(temp_list))]
-        immut_list = (list_2)
-        check_sum_verify(list_2)
-        print(immut_list)
-        #print([hex(list_2[x]) for x in range(len(list_2))])
+        check_sum_verify(tuple(list_2))
+        address = list_2[5]
+        del list_2[0:8]
+        del list_2[-1]
+        data = shift_add(list_2)
+        print(data)
+        if (address == 0x00):
+            print('')
+        elif (address == 0x01):
+            print('')
+        elif (address == 0x02):
+            print('')
+
     elif (ident == 'GPS1'):
         print(lists[x])
     elif (ident == 'Imag'):
         print(lists[x])
 
-#del list[0:9]
-#del list[101:103]
-#list_1 = [ ''.join(x) for x in zip(list[0::2], list[1::2]) ]
-#list_2 = [int(list_1[x], 16) for x in range(0, len(list_1)) ]
-#list_3 = [list_2[x]/2048 for x in range(0, len(list_2))]
-print(list)
-#print(len(list_1))
 csvfile.close()
