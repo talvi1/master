9b38830f39b3a7d3b5110af25de06f37e25aee0d
unknown <swerve0o0o0o@gmail.com>
2019-03-15 19:14:03 -0600

    loading images from database to websites

---
+++ Heng/loadimages.php
@@ -0,0 +1,33 @@
+<?php
+
+
+$servername = "localhost";
+$username = "roadqual_admin";
+$password = "admin";
+$dbname = "roadqual_capstone";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+
+$sql = "SELECT * FROM data_r_pi" ;
+$result = $conn->query($sql);
+
+    while($rows=mysqli_fetch_assoc($result))
+{
+
+   $image = $rows['originalImage'];    
+    print $image;
+
+
+
+}
+
+$conn->close();
+
+?>
